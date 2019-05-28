a8724846b30aff5cba33b4f81738ee7a86cf5f89
halilguler <halilguler1994@gmail.com>
2019-05-28 22:41:46 +0300

    Byt.txt fs.writeFile added file

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ fs.readFile('hello.txt',(err,data)=>{
 
 const file = fs.readFileSync('./hello.txt');
 console.log('Sync',file.toString('utf8'));
+
+//APPEND
+fs.appendFile('./hello.txt',' This is cool!',err=>{
+    if(err){
+        console.log(err);
+    }
+});
+
+//WRITE
+
+fs.writeFile('byt.txt','Hello New File',err=>{
+    if(err){
+        console.log(err);
+    }
+});
+
